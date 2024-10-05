9c8a19d0e02661e940309e6e70262bdbc7374fdf
Yonex <yonexbat@gmail.com>
2024-10-05 11:13:49 +0200

    feat: iterate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  firestore: Firestore = inject(Firestore);
+  items$: Observable<any[]>;
   title = 'shopping-app';
+
+  constructor() {
+    const aCollection = collection(this.firestore, 'shoppinglists')
+    this.items$ = collectionData(aCollection);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ "projectId": "shoppinglist-d0d2b", "appId": "1:579901157928:web:2314f1d9791842c7a67b1d", "databaseURL": "https://shoppinglist-d0d2b-default-rtdb.europe-west1.firebasedatabase.app", "storageBucket": "shoppinglist-d0d2b.appspot.com", "apiKey": "AIzaSyC8a6hEl5gLEvbAQpuDT6foIt3yyISp7s0", "authDomain": "shoppinglist-d0d2b.firebaseapp.com", "messagingSenderId": "579901157928", "measurementId": "G-1WH057YYMT" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
